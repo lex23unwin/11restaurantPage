e0334abbe5c9d5e8473314670974d99565b67011
lex23unwin <lex23unwin@gmail.com>
2023-08-18 16:44:28 -0500

    Basic tab functionality complete

--- main.js
+++ main.js
@@ -12,14 +12,15 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _home_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
 /* harmony import */ var _menu_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(3);
 /* harmony import */ var _contact_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(4);
+/* harmony import */ var _tabs_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(5);
+
 
 
 
 
 function pageLoad() {
+    (0,_tabs_js__WEBPACK_IMPORTED_MODULE_3__["default"])();
     (0,_home_js__WEBPACK_IMPORTED_MODULE_0__["default"])();
-    (0,_menu_js__WEBPACK_IMPORTED_MODULE_1__["default"])();
-    (0,_contact_js__WEBPACK_IMPORTED_MODULE_2__["default"])();
 }
 
 /***/ }),
@@ -75,6 +76,69 @@ function createContact() {
 
     content.appendChild(contactContent)
 }
+
+/***/ }),
+/* 5 */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ createTabs)
+/* harmony export */ });
+/* harmony import */ var _home_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
+/* harmony import */ var _menu_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(3);
+/* harmony import */ var _contact_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(4);
+
+
+
+
+function createTabs() {
+    const content = document.querySelector("#content")
+    
+    const tabsRow = document.createElement("div");
+    tabsRow.classList.add("tabsRow")
+
+    const homeTab = document.createElement("div");
+    homeTab.classList.add("tab")
+    homeTab.textContent = "homeTab"
+    homeTab.addEventListener("click", () => {
+        while (content.hasChildNodes()) {
+            content.removeChild(content.lastChild);
+        }
+        (0,_home_js__WEBPACK_IMPORTED_MODULE_0__["default"])();
+    })
+
+
+    const menuTab = document.createElement("div");
+    menuTab.classList.add("tab")
+    menuTab.textContent = "menuTab"
+    menuTab.addEventListener("click", () => {
+        while (content.hasChildNodes()) {
+            content.removeChild(content.lastChild);
+        }
+        (0,_menu_js__WEBPACK_IMPORTED_MODULE_1__["default"])();
+    })
+
+
+    const contactTab = document.createElement("div");
+    contactTab.classList.add("tab");
+    contactTab.textContent = "contactTab";
+    contactTab.addEventListener("click", () => {
+        while (content.hasChildNodes()) {
+            content.removeChild(content.lastChild);
+        }
+        (0,_contact_js__WEBPACK_IMPORTED_MODULE_2__["default"])();
+    })
+
+    tabsRow.appendChild(homeTab);
+    tabsRow.appendChild(menuTab);
+    tabsRow.appendChild(contactTab);
+
+    content.appendChild(tabsRow)
+}
+
+
+
 
 /***/ })
 /******/ 	]);
