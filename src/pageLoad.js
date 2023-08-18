864513dd876d1629bf3491c12d7aad92cdbde00d
lex23unwin <lex23unwin@gmail.com>
2023-08-18 16:44:28 -0500

    Basic tab functionality complete

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,9 +1,9 @@
 import createHome from './home.js'
 import createMenu from './menu.js'
 import createContact from './contact.js'
+import createTabs from './tabs.js'
 
 export default function pageLoad() {
+    createTabs();
     createHome();
-    createMenu();
-    createContact();
 }
---
+++ src/tabs.js
@@ -0,0 +1,50 @@
+import createHome from './home.js'
+import createMenu from './menu.js'
+import createContact from './contact.js'
+
+export default function createTabs() {
+    const content = document.querySelector("#content")
+    
+    const tabsRow = document.createElement("div");
+    tabsRow.classList.add("tabsRow")
+
+    const homeTab = document.createElement("div");
+    homeTab.classList.add("tab")
+    homeTab.textContent = "homeTab"
+    homeTab.addEventListener("click", () => {
+        while (content.hasChildNodes()) {
+            content.removeChild(content.lastChild);
+        }
+        createHome();
+    })
+
+
+    const menuTab = document.createElement("div");
+    menuTab.classList.add("tab")
+    menuTab.textContent = "menuTab"
+    menuTab.addEventListener("click", () => {
+        while (content.hasChildNodes()) {
+            content.removeChild(content.lastChild);
+        }
+        createMenu();
+    })
+
+
+    const contactTab = document.createElement("div");
+    contactTab.classList.add("tab");
+    contactTab.textContent = "contactTab";
+    contactTab.addEventListener("click", () => {
+        while (content.hasChildNodes()) {
+            content.removeChild(content.lastChild);
+        }
+        createContact();
+    })
+
+    tabsRow.appendChild(homeTab);
+    tabsRow.appendChild(menuTab);
+    tabsRow.appendChild(contactTab);
+
+    content.appendChild(tabsRow)
+}
+
+
