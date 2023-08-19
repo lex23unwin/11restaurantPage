6f99d4eb5976a8f898bdf9041efd9b82782bd4b5
lex23unwin <lex23unwin@gmail.com>
2023-08-18 21:12:50 -0500

    Trying to get github pages working with dist folder

--- dist/main.js
+++ dist/main.js
@@ -244,7 +244,7 @@ function createFooter() {
     const content = document.querySelector("#content")
     const footer = document.createElement("div");
     footer.classList.add("footer")
-    footer.textContent = "Alexander Unwin - The Odin Project - Summer 2023";
+    footer.textContent = "Alexander Unwin -- The Odin Project -- Summer 2023";
     content.appendChild(footer)
 }
 
